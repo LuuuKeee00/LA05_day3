6a98e503e7e268bba9b451508d2e16c9000fc91b
Luuukeee08 <108565244+Luuukeee08@users.noreply.github.com>
2022-09-12 09:26:39 +0900

    new

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function ($) {
+  $('.bg-swicher').bgSwitcher({
+    images: ['images/background1.jpg', 'images/background2.jpg', 'images/background3.jpg', 'images/background4.jpg', 'images/background5.jpg'],
+    interval: 5000,
+    loop: true
+  });
+
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 2, 
+  slidesToScroll: 1
+});
+
+  $(document).ready(function(){
+    $('.fuwat').css('visibility','hidden');
+    $(window).scroll(function(){
+     var windowHeight = $(window).height(),
+         topWindow = $(window).scrollTop();
+     $('.fuwat').each(function(){
+      var objectPosition = $(this).offset().top;
+      if(topWindow > objectPosition - windowHeight + 200){
+       $(this).addClass("fuwatAnime");
+      }
+     });
+    });
+  });
+
+});
+
